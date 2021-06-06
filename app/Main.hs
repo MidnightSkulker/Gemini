27bc75b90ddb2670bac0e9890dff58ccdfb233e4
Peter White <desourdesourde@gmail.com>
2021-06-05 18:02:07 -0700

    Basic web server up and running

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,19 @@
 module Main where
 
 import Lib
+import Debug.Trace
 import Web.Scotty
 import Network.HTTP.Types
 
-main :: IO ()
-main = someFunc
+main = scotty 3000 $ do
+  get "/" $ do                         -- handle GET request on "/" URL
+    trace "GET request!" (text "This was a GET request!")     -- send 'text/plain' response
+  delete "/" $ do
+    html "This was a DELETE request!"  -- send 'text/html' response
+  post "/" $ do
+    text "This was a POST request!"
+  put "/" $ do
+    text "This was a PUT request!"
+
+-- main :: IO ()
+-- main = someFunc
