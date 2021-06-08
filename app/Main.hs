d7feff09185f5edc2bb57b00d9590ab707385cdb
Peter White <desourdesourde@gmail.com>
2021-06-08 12:49:36 -0700

    Better service for html

--- app/Main.hs
+++ app/Main.hs
@@ -5,26 +5,41 @@ import Lib
 import Debug.Trace
 import Web.Scotty
 import Network.HTTP.Types
+import Network.Wai
 import Data.Aeson
 import Control.Monad
+import Control.Monad.IO.Class
+
+import qualified Data.ByteString as B
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as L
+import qualified Data.ByteString.Char8 as C
 
 readLazyByteStringFile :: String -> IO L.Text
 readLazyByteStringFile fileName = do
   str :: String <- readFile fileName
   return (L.pack str)
 
+-- Serve an HTML
+serveHtml :: ActionM ()
+serveHtml = do
+  r <- request
+  let rawpath :: String = C.unpack (rawPathInfo r)
+      path :: String = "html" ++ rawpath ++ ".html"
+  liftIO (putStrLn (("serveHtml --> ") ++ path))
+  setHeader "Content-Type" "text/html"
+  file path
+
 main :: IO ()
 main = do
   -- TODO: Should protect this with exception handling
   homeHtml :: L.Text <- readLazyByteStringFile "html/pout-jersey.html"
-  mainCss :: L.Text <- readLazyByteStringFile "assets/stylesheets/main.css"
   scotty 3000 $ do
-    -- The jobcoin homepage
-    get "/pout-jersey" $ do
-      r <- request
-      trace "Jobcoin home page\n" (html homeHtml)
+    -- Get other html files
+    get (regex "^/(.*).html$") $ do
+      setHeader "Content-Type" "text/html"
+      path <- param "0"
+      trace ("/html/" ++ path) (file ("html/" ++ path)) -- a la Sinatra
     -- Get css files from assets/stylesheets
     get "/assets/stylesheets/:css" $ do
       setHeader "Content-Type" "text/css"
@@ -32,23 +47,12 @@ main = do
       css <- param "css"
       trace ("/assets/stylesheets/" ++ css)
             (file ("assets/stylesheets/" ++ css))
+    -- Get html files without a .html suffix on the link
+    get "/pout-jersey" $ do trace "Jobcoin home page\n" serveHtml
     get "/" $ do                         -- handle GET request on "/" URL
       trace "GET request!\n" (text "This was a GET request!\n") -- send 'text/plain' response
       ps <- params
       trace "NULL check\n" (text (if null ps then "NULL parameters\n" else (fst (head ps))))
-    delete "/" $ do
-      trace "DELETE request!\n" (html "This was a DELETE request!\n")  -- send 'text/html' response
-    post "/" $ do
-      trace "POST request!\n" (text "This was a POST request!\n")
-    put "/" $ do
-      trace "PUT request!\n" (text "This was a PUT request!\n")
-    get "/one" $ do
-      -- status status302  -- Respond with HTTP 302 status code
-      -- setHeader "Location" "http://lonliest-number"
-      trace "GET/one request!\n" (text "This was a GET/one request!\n")     -- send 'text/plain' response
-  --  post "/set-headers" $ do
-  --    trace "POST /set_headers location 302\n" (status status302)  -- Respond with HTTP 302 status code
-  --    setHeader "Location" "http://www.google.com.au"
     -- named parameters:
     get "/askfor/:word\n" $ do
       w <- param "word"
