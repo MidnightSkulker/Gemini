5ee508ae08c6261bec6a31d9e33e4ed3f8866786
Peter White <desourdesourde@gmail.com>
2021-06-08 14:49:14 -0700

    Serving pout-jersey/addresses

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,7 @@ import Debug.Trace
 import Web.Scotty
 import Network.HTTP.Types
 import Network.Wai
-import Data.Aeson
+import Data.Aeson as Aeson
 import Control.Monad
 import Control.Monad.IO.Class
 
@@ -48,28 +48,18 @@ main = do
       trace ("/assets/stylesheets/" ++ css)
             (file ("assets/stylesheets/" ++ css))
     -- Get html files without a .html suffix on the link
+    -- I was having trouble getting apache to serve /pout-jersey,
+    -- pout-jersey/api, and so on.
     get "/pout-jersey" $ do trace "Jobcoin home page\n" serveHtml
     get "/pout-jersey/api" $ do trace "API home page\n" serveHtml
-    get "/" $ do                         -- handle GET request on "/" URL
-      trace "GET request!\n" (text "This was a GET request!\n") -- send 'text/plain' response
-      ps <- params
-      trace "NULL check\n" (text (if null ps then "NULL parameters\n" else (fst (head ps))))
-    -- named parameters:
-    get "/askfor/:word\n" $ do
-      w <- param "word"
-      html $ mconcat ["<h1>You asked for ", w, ", you got it!</h1>" ]
-    -- unnamed parameters from a query string or a form:
-    post "/submit" $ do  -- e.g. http://server.com/submit?name=somename
-      name <- param "name"
-      text name
-    -- match a route regardless of the method
-  --  trace "ANY /all" (matchAny "/all") $ do
-  --    text "matches all methods"
-        -- handler for when there is no matched route
-        -- (this should be the last handler because it matches all routes)
+    get "/pout-jersey/addresses/:addr" $ do
+      setHeader "Content-Type" "application/json"
+      addr :: String <- param "addr"
+      let addrstr :: String = "addesses/" ++ addr
+      text (L.pack addrstr)
     notFound $ do
       r <- request
       trace ("No such route\n" ++ show r) (text "There is no such route.\n")
-
+    -- Handle request to get information on a user/addresses
 -- main :: IO ()
 -- main = someFunc
