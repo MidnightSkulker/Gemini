78b0bd2f83902f747ca77e9d471ea47d57d8f6b6
Peter White <desourdesourde@gmail.com>
2021-06-07 22:47:04 -0700

    Able to serve .css files

--- app/Main.hs
+++ app/Main.hs
@@ -10,26 +10,38 @@ import Control.Monad
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as L
 
+readLazyByteStringFile :: String -> IO L.Text
+readLazyByteStringFile fileName = do
+  str :: String <- readFile fileName
+  return (L.pack str)
+
 main :: IO ()
 main = do
   -- TODO: Should protect this with exception handling
-  poutJerseyHtmlStr :: String <- readFile "html/pout-jersey.html"
-  let poutJerseyHtmlText :: L.Text = L.pack poutJerseyHtmlStr
-  putStrLn (take 100 poutJerseyHtmlStr)
+  homeHtml :: L.Text <- readLazyByteStringFile "html/pout-jersey.html"
+  mainCss :: L.Text <- readLazyByteStringFile "assets/stylesheets/main.css"
   scotty 3000 $ do
     -- The jobcoin homepage
     get "/pout-jersey" $ do
-      trace "Jobcoin home page\n" (html poutJerseyHtmlText)
+      r <- request
+      trace "Jobcoin home page\n" (html homeHtml)
+    -- Get css files from assets/stylesheets
+    get "/assets/stylesheets/:css" $ do
+      setHeader "Content-Type" "text/css"
+      -- Sinatra style route and capture.
+      css <- param "css"
+      trace ("/assets/stylesheets/" ++ css)
+            (file ("assets/stylesheets/" ++ css))
     get "/" $ do                         -- handle GET request on "/" URL
       trace "GET request!\n" (text "This was a GET request!\n") -- send 'text/plain' response
       ps <- params
-      text (if null ps then "NULL\n" else (fst (head ps)))
-  --  delete "/" $ do
-  --    trace "DELETE request!\n" (html "This was a DELETE request!\n")  -- send 'text/html' response
-  --  post "/" $ do
-  --    trace "POST request!\n" (text "This was a POST request!\n")
-  --  put "/" $ do
-  --    trace "PUT request!\n" (text "This was a PUT request!\n")
+      trace "NULL check\n" (text (if null ps then "NULL parameters\n" else (fst (head ps))))
+    delete "/" $ do
+      trace "DELETE request!\n" (html "This was a DELETE request!\n")  -- send 'text/html' response
+    post "/" $ do
+      trace "POST request!\n" (text "This was a POST request!\n")
+    put "/" $ do
+      trace "PUT request!\n" (text "This was a PUT request!\n")
     get "/one" $ do
       -- status status302  -- Respond with HTTP 302 status code
       -- setHeader "Location" "http://lonliest-number"
@@ -51,7 +63,8 @@ main = do
         -- handler for when there is no matched route
         -- (this should be the last handler because it matches all routes)
     notFound $ do
-      text "There is no such route.\n"
+      r <- request
+      trace ("No such route\n" ++ show r) (text "There is no such route.\n")
 
 -- main :: IO ()
 -- main = someFunc
