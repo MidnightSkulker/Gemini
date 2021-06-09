38271ba5c6d015179c57258d7b235e9bb7722ea2
Peter White <desourdesourde@gmail.com>
2021-06-09 13:21:29 -0700

    Eliminated debugging traces

--- app/Main.hs
+++ app/Main.hs
@@ -6,9 +6,11 @@ import Debug.Trace
 import Web.Scotty
 import Network.HTTP.Types
 import Network.Wai
+import Network.Wai.Middleware.RequestLogger
 import Data.Aeson as Aeson
 import Control.Monad
 import Control.Monad.IO.Class
+import qualified Control.Monad.Trans.State as ST
 import Text.Blaze.Html hiding (text) -- Conflicts with Scotty
 
 import qualified Data.ByteString as B
@@ -18,6 +20,7 @@ import qualified Data.ByteString.Char8 as C
 import Data.Time.Clock
 import Coins
 
+-- Read a file into a Text string
 readLazyByteStringFile :: String -> IO L.Text
 readLazyByteStringFile fileName = do
   str :: String <- readFile fileName
@@ -33,28 +36,30 @@ serveHtml = do
   setHeader "Content-Type" "text/html"
   file path
 
+-- type ScottyST = ST.StateT String ScottyM ()
+-- scottyST :: ScottyST -> IO () = liftM scotty
+
 main :: IO ()
 main = do
-  -- TODO: Should protect this with exception handling
   homeHtml :: L.Text <- readLazyByteStringFile "html/pout-jersey.html"
   scotty 3000 $ do
+    middleware logStdoutDev
     -- Get other html files
     get (regex "^/(.*).html$") $ do
       setHeader "Content-Type" "text/html"
       path <- param "0"
-      trace ("/html/" ++ path) (file ("html/" ++ path)) -- a la Sinatra
+      file ("html/" ++ path) -- a la Sinatra
     -- Get css files from assets/stylesheets
     get "/assets/stylesheets/:css" $ do
       setHeader "Content-Type" "text/css"
       -- Sinatra style route and capture.
       css <- param "css"
-      trace ("/assets/stylesheets/" ++ css)
-            (file ("assets/stylesheets/" ++ css))
+      file ("assets/stylesheets/" ++ css)
     -- Get html files without a .html suffix on the link
     -- I was having trouble getting apache to serve /pout-jersey,
     -- pout-jersey/api, and so on.
-    get "/pout-jersey" $ do trace "Jobcoin home page\n" serveHtml
-    get "/pout-jersey/api" $ do trace "API home page\n" serveHtml
+    get "/pout-jersey" $ do serveHtml
+    get "/pout-jersey/api" $ do serveHtml
     get "/pout-jersey/addresses/:addr" $ do
       setHeader "Content-Type" "application/json"
       addr :: String <- param "addr"
@@ -62,7 +67,7 @@ main = do
       text (L.pack addrstr)
     notFound $ do
       r <- request
-      trace ("No such route\n" ++ show r) (text "There is no such route.\n")
+      text "There is no such route.\n"
     -- Handle request to get information on a user/addresses
 -- main :: IO ()
 -- main = someFunc
