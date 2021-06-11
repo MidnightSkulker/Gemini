44a10950a03c64e32beb65da2d4a6baefb646afa
Peter White <desourdesourde@gmail.com>
2021-06-11 11:27:45 -0700

    Getting # of jobcoins works

--- src/Coins.hs
+++ src/Coins.hs
@@ -36,13 +36,13 @@ type LedgerEntry = (Address, Amount)
 
 -- The requirements of the coding demo is that the ledger needs
 -- to be displayable on a web page. So our ledger will have
--- only a small number of entries, not thousands or millions of
--- entries. Nevertheless, we will consider efficient of lookups
+-- only a small number of items, not thousands or millions of
+-- items. Nevertheless, we will consider efficient of lookups
 -- and updates in the ledger by implementing it as a Map (i.e. a
 -- hash table), not a list. That way updates and searches will
 -- take nearly constant time.
 data Ledger =
-  Ledger {entries :: Map.Map Address Amount} deriving (Show, Generic, ToJSON)
+  Ledger {items :: Map.Map Address Amount} deriving (Show, Generic, ToJSON)
 
 -- Format a ledger entry for HTML
 ledgerLine :: LedgerEntry -> Html
@@ -61,38 +61,40 @@ tableHeader col1hdr col2hdr = do
       th (text col1hdr)
       th (text col2hdr)
 
--- Format the legder table for Html
+-- Create the legder table for Html
 -- mapM :: (Taversable t, Monad m) => (a -> m b) -> t a -> m (t b)
 -- For formatting the web page, I must assume the ledger does not
--- have a zillion entries, so that it is reasonable to Send
+-- have a zillion items, so that it is reasonable to Send
 -- the ledger using http and displaying it on your browser.
 -- This I convert the ledger to an association list in preparation
 -- for display.
+-- I chose to use HTML combinators since we are generating HTML.
 ledger2html :: Ledger -> Html
 ledger2html ledger =
-  let numberOfEntries :: Int = length (entries ledger)
-      numberOfEntriesHtml :: Html = text (pack (show (numberOfEntries) ++ " Addresses"))
+  let numberOfItems :: Int = length (items ledger)
+      numberOfItemsHtml :: Html = text (pack (show (numberOfItems) ++ " Addresses"))
   in do
       h3 ! class_ "ui header" $ "Current Balances"
       table ! class_ "ui collapsing table segment" $ do
         tableHeader "Address" "Balance"
         tbody $ do
-          ledgerLine ("Gronk", 0.0)
-          foldMap ledgerLine (Map.assocs (entries ledger))
+          ledgerLine ("Gronk", 0.0) -- TODO: Remove this line
+          -- Produce the list of ledger items
+          foldMap ledgerLine (Map.assocs (items ledger))
         tfoot $ do
           tr $ do
-            td ! colspan "2" $ numberOfEntriesHtml
+            td ! colspan "2" $ numberOfItemsHtml
 
 -- Initial value of the Ledger
 emptyLedger :: Ledger
-emptyLedger = Ledger { entries = Map.empty }
+emptyLedger = Ledger { items = Map.empty }
 
 -- Add value to a user in the ledger
 -- adjust :: Ord k => (a -> a) -> k -> Map k a -> Map k a
 addValue :: Amount -> Address -> Ledger -> Ledger
 addValue amount addr ledger =
-  Ledger {entries = Map.insertWith (+) addr amount (entries ledger)}
+  Ledger {items = Map.insertWith (+) addr amount (items ledger)}
 
 -- findWithDefault :: Ord k => a -> k -> Map k a -> a
 getValue :: Address -> Ledger -> Amount
-getValue addr ledger = Map.findWithDefault 0.0 addr (entries ledger)
+getValue addr ledger = Map.findWithDefault 0.0 addr (items ledger)
